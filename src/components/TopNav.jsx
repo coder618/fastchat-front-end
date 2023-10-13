dd39479fa662cef46c1de5f95434aa9a397a0397
Ahadul islam Mint <iamahadul@gmail.com>
2023-10-13 09:19:48 +0600

    mobile responsive done

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -47,6 +47,28 @@ export default function TopNav({ currentUserData, logout, isConnected }) {
                     </div>
                 </nav>
             </div>
+
+            <div className="on-mobile-status">
+                <div className="left-part">
+                    <div className="welcome-message">Welcome, {userName}</div>
+                </div>
+                <div className="right-part">
+                    <div className="connection-status">
+                        {isConnected ? (
+                            <span className="connected badge bg-success">Connected</span>
+                        ) : (
+                            <div className="not-connected">
+                                <p>Backend booting up please wait 30-60s for first time load</p>
+
+                                <div className="badge bg-info">
+                                    Connecting
+                                    <img src={`loading.svg`} alt="loading" />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
